6cc39796dd6839593cf95897b440e9d9bdbb7fd7
NONruto <hannesbleidorn@gmail.com>
2025-01-21 14:18:37 +0100

    commit

--- logisticFront/src/app/app.routes.ts
+++ logisticFront/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { ArticleComponent } from './article/article.component';
 import { LoginComponent } from './login/login.component';
 import { CustomerOverviewComponent } from './customer-overview/customer-overview.component';
 import { CustomerOrderComponent } from './customer-order/customer-order.component';
+import { OrderComponent } from './order/order.component';
 
 export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'articles', component: ArticleComponent },
     { path: 'customer', component: CustomerOverviewComponent },
+    { path: 'order', component: OrderComponent },
     { path: 'customerOrder', component: CustomerOrderComponent },
     { path: '', component: LoginComponent },
     { path: '**', redirectTo: '' }
--- logisticFront/src/app/order/order.component.ts
+++ logisticFront/src/app/order/order.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Order, Product, GidLogisticsService, Message } from '../../../swagger';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-order',
-  standalone: true,
-  imports: [],
   templateUrl: './order.component.html',
-  styleUrl: './order.component.css'
+  styleUrls: ['./order.component.css'],
+  providers: [GidLogisticsService],
+  standalone: true,
+  imports: [TableModule, DialogModule, ButtonModule, CommonModule],
+  styles: [`
+    :host {
+      @apply h-full flex flex-col;
+    }
+  `],
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  orders: Order[] = [];
+  selectedOrder: Order | null = null;
+  orderDialog: boolean = false;
 
+  constructor(private logisticsService: GidLogisticsService) {}
+
+  ngOnInit(): void {
+    this.loadOrders();
+  }
+
+  loadOrders(): void {
+    this.logisticsService.apiGidLogisticsOrdersGet().subscribe((data: Message) => {
+      this.orders = data.body || [];
+    });
+  }
+
+  viewOrderDetails(order: Order): void {
+    this.selectedOrder = order;
+    this.orderDialog = true;
+  }
+
+  hideOrderDialog(): void {
+    this.orderDialog = false;
+    this.selectedOrder = null;
+  }
 }
